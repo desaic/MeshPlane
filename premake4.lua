a3fbdd081cd1a26296de8fb4ba1a3c13060c8004
Desai Chen <desaic@csail.mit.edu>
2013-06-03 00:21:35 -0400

    restore misisng src files

--- premake4.lua
+++ premake4.lua
@@ -10,6 +10,7 @@ includedirs{"include"}
 libdirs{"lib"}
 links{"GLEW", "GLU", "glut", "GL"}
 links{"mesh_query", "png", "pthread"}
+buildoptions { "-std=c++0x"}
 configuration {"Debug"}
               defines{"Debug"}
               flags{"Symbols"}
